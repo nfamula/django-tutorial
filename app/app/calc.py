e190e40bdf1a9e23677781e3c5578597b027fe0a
Natalie Famula <nfamula@gmail.com>
2024-04-29 09:08:25 -0400

    add another test

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,5 +3,9 @@ Calculator functions
 """
 
 def add(x, y):
-    """Add two numbers together"""
+    """Add two numbers together."""
     return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return 5
