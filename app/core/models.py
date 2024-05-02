b19a125a1fa3ef712498dc30d9ee652d9d06cbe1
Natalie Famula <nfamula@gmail.com>
2024-05-02 16:32:29 -0400

    fix missing whitespace

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@
 Database models.
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
