12eeef5b754bb23b05dfff420e9e5c8665284e55
Natalie Famula <nfamula@gmail.com>
2024-05-28 09:50:37 -0400

    enable user app

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o-*m)*9p*$78+nog@)da&4kh=5w08(wk4(1mc!a2kvi94)bvjy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'drf_spectacular',
+    'user',
     'core',
 ]
 
